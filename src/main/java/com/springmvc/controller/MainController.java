f8aeadc3c52a72502bd8838d292619ae4a8a2fc7
anyang <anyang_zhu@126.com>
2017-09-06 11:23:40 +0800

    ssm整合fastjson、logbackk

--- src/main/java/com/springmvc/controller/MainController.java
+++ src/main/java/com/springmvc/controller/MainController.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 /**
  * Created by zhujiating on 2017/6/27.
@@ -21,6 +24,8 @@ public class MainController {
     @Autowired
     UserServiceImpl userServiceImpl ;
 
+    HashMap hashMap = new HashMap();
+
     @RequestMapping(value = "/hello",method={RequestMethod.POST,RequestMethod.GET})
     public String index(HttpServletRequest request,Model model){
         User user = userServiceImpl.loadUserById(1);
@@ -28,5 +33,19 @@ public class MainController {
         return "/hello";
     }
 
+
+    @ResponseBody
+    @RequestMapping(value = "/requestJson",method = {RequestMethod.GET,RequestMethod.POST})
+    public User rquestJson(HttpServletRequest request, HttpServletResponse response){
+        User user = new User();
+        user.setFirstName("anyang");
+        user.setId(1);
+        user.setLastName("an");
+        user.setNickName("anyangyang");
+//        HashMap<String,String> map = new HashMap<String,String>();
+//        map.put("name","anyang");
+
+        return user;
+    }
 
 }
